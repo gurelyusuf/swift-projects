323bf3308cd74afbc466f7936e3d1303d93ad5ee
Yusuf GÜREL <yusufgurel@outlook.com>
2023-05-26 14:19:44 +0300

    Add files via upload

---
+++ DarkModeProject/DarkModeProject/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  DarkModeProject
+//
+//  Created by GUREL on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var changeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //overrideUserInterfaceStyle = .light
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.blue
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.blue
+        }
+    }
+
+
+}
+
