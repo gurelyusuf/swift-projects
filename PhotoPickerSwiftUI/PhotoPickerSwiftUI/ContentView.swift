364d8d61257aacd513018d3a505c6c2ac58e386f
Yusuf GÜREL <yusufgurel@outlook.com>
2023-06-20 10:04:06 +0300

    Add files via upload

---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUI/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by GUREL on 20.06.2023.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ContentView: View {
+    
+    @State var selectedItem : [PhotosPickerItem] = []
+    @State var data : Data?
+    
+    var body: some View {
+        VStack {
+            if let data = data {
+                if let selectedImage = UIImage(data: data) {
+                    Spacer()
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .frame(width: 300,height: 250,alignment: .center)
+                }
+            }
+            
+            Spacer()
+            
+            PhotosPicker(selection: $selectedItem, maxSelectionCount: 1, selectionBehavior: .default, matching: .images) {
+                Text("Select Image")
+            }.onChange(of: selectedItem) { newValue in
+                guard let item = selectedItem.first else {
+                    return
+                }
+                item.loadTransferable(type: Data.self) { result in
+                    switch result {
+                    case.success(let data):
+                        if let data = data {
+                            self.data = data
+                        }
+                    case.failure(let error):
+                        print(error)
+                    }
+                }
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUI/PhotoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSwiftUIApp.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by GUREL on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
